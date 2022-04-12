91c2b10563fb93141fa3e10927c3dfd7962fe65a
Earthbert <daraban_timotei@tutanota.com>
2022-04-12 08:51:51 +0300

    added ll_print in header

--- LinkedList.h
+++ LinkedList.h
@@ -1,20 +1,18 @@
 #ifndef __LINKED_LIST_H_
 #define __LINKED_LIST_H_
 
-typedef struct ll_node_t ll_node_t;
-struct ll_node_t
+typedef struct
 {
     void* data;
     ll_node_t* next;
-};
+} ll_node_t;
 
-typedef struct linked_list_t linked_list_t;
-struct linked_list_t
+typedef struct
 {
     ll_node_t* head;
     unsigned int data_size;
     unsigned int size;
-};
+} linked_list_t;
 
 linked_list_t*
 ll_create(unsigned int data_size);
@@ -32,9 +30,6 @@ void
 ll_free(linked_list_t* pp_list);
 
 void
-ll_print_int(linked_list_t* list);
-
-void
-ll_print_string(linked_list_t* list);
+ll_print(linked_list_t* list, void *print_func(void *));
 
 #endif /* __LINKED_LIST_H_ */
